cbd5634f17e45892cab4e6079f3416c86722a46f
JackDanna <jackdanna78@gmail.com>
2026-05-29 10:50:43 -0400

    Added ObservableCach to it's own repo

---
+++ ObservableCache.fs
@@ -0,0 +1,201 @@
+module ObservableCache
+
+open System
+open System.Collections.Concurrent
+open System.Reactive.Subjects
+open FSharp.Control.Reactive
+
+type CacheInput<'Item, 'ItemId, 'ItemMsg> =
+    | CreateItem of 'ItemId * 'Item
+    | ReadItem of 'ItemId
+    | UpdateItem of 'ItemId * 'ItemMsg
+    | DeleteItem of 'ItemId
+
+type Input<'Item, 'ItemId, 'ItemMsg> = {
+    CorrelationId: Guid
+    CacheInput: CacheInput<'Item, 'ItemId, 'ItemMsg>
+}
+
+type CacheOutput<'ItemId, 'Item> =
+    | PersistItemOnDB of 'ItemId * Result<'Item, string>
+    | DeleteItemOnDB of 'ItemId * Result<unit, string>
+
+type Output<'ItemId, 'Item> = {
+    CorrelationId: Guid
+    CacheOutput: CacheOutput<'ItemId, 'Item>
+}
+
+let obsCache
+    (createOrUpdateItemOnDatabase: 'Item -> IObservable<Result<'Item, string>>)
+    (readItemOnDatabase: 'ItemId -> IObservable<Result<'Item, string>>)
+    (deleteItemOnDatabase: 'ItemId -> IObservable<Result<unit, string>>)
+    (updateItem: 'ItemMsg -> 'Item -> 'Item)
+    (evictionDelay: TimeSpan)
+    (inputObservable: IObservable<Input<'Item, 'ItemId, 'ItemMsg>>)
+    =
+    let itemCache = ConcurrentDictionary<'ItemId, 'Item>()
+
+    let getItem itemId =
+        match itemCache.TryGetValue itemId with
+        | true, item -> Ok item |> Observable.single
+        | false, _ ->
+            readItemOnDatabase itemId
+            |> Observable.map (
+                Result.bind (fun item ->
+                    match itemCache.TryAdd(itemId, item) with
+                    | true -> Ok item
+                    | false -> Error "Failed to add item to cache")
+            )
+        |> Observable.map (fun result -> itemId, result)
+
+    inputObservable
+    |> Observable.groupByUntil
+        (_.CacheInput
+         >> function
+             | CreateItem(itemId, _) -> itemId
+             | ReadItem itemId -> itemId
+             | UpdateItem(itemId, _) -> itemId
+             | DeleteItem itemId -> itemId)
+        (fun group ->
+            Observable.merge
+                (group
+                 |> Observable.filter (
+                     _.CacheInput
+                     >> function
+                         | DeleteItem _ -> true
+                         | _ -> false
+                 ))
+                (group |> Observable.throttle evictionDelay))
+    |> Observable.bind (fun groupedObservable ->
+        let outputObservable =
+            groupedObservable
+            |> Observable.bind (fun input ->
+                match input.CacheInput with
+                | CreateItem(itemId, item) ->
+                    (itemId,
+                     match itemCache.TryAdd(itemId, item) with
+                     | true -> Ok item
+                     | false -> Error "Item already exists in cache")
+                    |> Observable.single
+                    |> Observable.map PersistItemOnDB
+                | ReadItem itemId -> getItem itemId |> Observable.map PersistItemOnDB
+                | UpdateItem(itemId, itemMsg) ->
+                    getItem itemId
+                    |> Observable.map (fun (itemId, itemResult) ->
+                        itemId,
+                        itemResult
+                        |> Result.bind (fun item ->
+                            let updatedItem = updateItem itemMsg item
+
+                            match itemCache.TryUpdate(itemId, updatedItem, item) with
+                            | true -> Ok updatedItem
+                            | false -> Error "Failed to update item in cache"))
+                    |> Observable.map PersistItemOnDB
+                | DeleteItem itemId ->
+                    (itemId,
+                     match itemCache.TryRemove itemId with
+                     | true, _ -> Ok()
+                     | false, _ -> Ok())
+                    |> Observable.single
+                    |> Observable.map DeleteItemOnDB
+                |> Observable.map (fun co -> input.CorrelationId, co))
+            |> Observable.publish
+            |> Observable.refCount
+
+        outputObservable
+        |> Observable.takeLast 1
+        |> Observable.bind (fun (correlationId, cacheOutput) ->
+            match cacheOutput with
+            | PersistItemOnDB(itemId, result) ->
+                match result with
+                | Error err -> (itemId, Error err) |> Observable.single
+                | Ok item ->
+                    item
+                    |> createOrUpdateItemOnDatabase
+                    |> Observable.map (fun result ->
+                        itemId,
+                        result
+                        |> Result.bind (fun databaseItem ->
+                            match itemCache.TryRemove itemId with
+                            | true, _ -> Ok databaseItem
+                            | false, _ -> Error "Failed to remove item from cache after persist"))
+                |> Observable.map PersistItemOnDB
+            | DeleteItemOnDB(itemId, result) ->
+                match result with
+                | Error err -> (itemId, Error err) |> Observable.single
+                | Ok() ->
+                    deleteItemOnDatabase itemId
+                    |> Observable.map (fun result ->
+                        itemId,
+                        match result with
+                        | Ok() -> Ok()
+                        | Error err -> Error err)
+                |> Observable.map DeleteItemOnDB
+            |> Observable.map (fun co -> {
+                CorrelationId = correlationId
+                CacheOutput = co
+            }))
+        |> Observable.subscribe (fun output -> ())
+        |> ignore
+
+        outputObservable)
+
+let createHelperFunctions
+    (createOrUpdateItemOnDatabase: 'Item -> IObservable<Result<'Item, string>>)
+    (readItemOnDatabase: 'ItemId -> IObservable<Result<'Item, string>>)
+    (deleteItemOnDatabase: 'ItemId -> IObservable<Result<unit, string>>)
+    (updateItem: 'ItemMsg -> 'Item -> 'Item)
+    (evictionDelay: TimeSpan)
+    =
+    let inputSubject = new Subject<Input<'Item, 'ItemId, 'ItemMsg>>()
+
+    let pendingItemRequests =
+        ConcurrentDictionary<Guid, System.Threading.Tasks.TaskCompletionSource<Result<'Item, string>>>()
+
+    let pendingDeleteRequests =
+        ConcurrentDictionary<Guid, System.Threading.Tasks.TaskCompletionSource<Result<unit, string>>>()
+
+    let temp =
+        obsCache
+            createOrUpdateItemOnDatabase
+            readItemOnDatabase
+            deleteItemOnDatabase
+            updateItem
+            evictionDelay
+            inputSubject
+        |> Observable.subscribe (fun (correlationGuid, output) ->
+            match output with
+            | PersistItemOnDB(_, result) ->
+                match pendingItemRequests.TryRemove correlationGuid with
+                | true, tcs -> tcs.SetResult result
+                | false, _ -> ()
+            | DeleteItemOnDB(_, result) ->
+                match pendingDeleteRequests.TryRemove correlationGuid with
+                | true, tcs -> tcs.SetResult result
+                | false, _ -> ())
+
+    let dispatch msg =
+        let correlationId = Guid.NewGuid()
+        let tcs = System.Threading.Tasks.TaskCompletionSource<Result<'Item, string>>()
+        pendingItemRequests[correlationId] <- tcs
+
+        inputSubject.OnNext {
+            CorrelationId = correlationId
+            CacheInput = msg
+        }
+
+        tcs.Task
+
+    let dispatchDelete msg =
+        let correlationId = Guid.NewGuid()
+        let tcs = System.Threading.Tasks.TaskCompletionSource<Result<unit, string>>()
+        pendingDeleteRequests[correlationId] <- tcs
+
+        inputSubject.OnNext {
+            CorrelationId = correlationId
+            CacheInput = msg
+        }
+
+        tcs.Task
+
+    CreateItem >> dispatch, ReadItem >> dispatch, UpdateItem >> dispatch, DeleteItem >> dispatchDelete
