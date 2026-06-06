284c8aad647d9d518aeffb030ff6d30b02fd95de
JackDanna <jackdanna78@gmail.com>
2026-06-06 08:09:12 -0400

    Inlining our new logic

--- ObservableCache.fs
+++ ObservableCache.fs
@@ -102,6 +102,91 @@ let temp (observableOfInput: IObservable<Input<'Item, 'ItemId, 'ItemMsg>>) (read
     |> AsyncSeq.toObservable
     |> Observable.choose (fun ( _, processorOutputs) -> processorOutputs)
 
+let obsCache2
+    (createOrUpdateItemOnDatabase: 'Item -> Async<Result<'Item, string>>)
+    (readItemOnDatabase: 'ItemId -> Async<Result<'Item, string>>)
+    (deleteItemOnDatabase: 'ItemId -> Async<Result<unit, string>>)
+    (updateItem: 'ItemMsg -> 'Item -> 'Item)
+    (evictionDelay: TimeSpan)
+    (inputObservable: IObservable<Input<'Item, 'ItemId, 'ItemMsg>>)
+    =
+
+    inputObservable
+    |> Observable.groupByUntil
+        (_.CacheInput
+          >> function
+             | CreateItem(itemId, _) -> itemId
+             | ReadItem itemId -> itemId
+             | UpdateItem(itemId, _) -> itemId
+             | DeleteItem itemId -> itemId)
+        (fun group ->
+            Observable.merge
+                (group
+                 |> Observable.filter (
+                     _.CacheInput
+                     >> function
+                         | DeleteItem _ -> true
+                         | _ -> false
+                 ))
+                (group |> Observable.throttle evictionDelay))
+    |> Observable.bind (
+            AsyncSeq.ofObservableBuffered
+            >> AsyncSeq.scanAsync 
+                (
+                    fun (state: 'Item option, cacheOutput:Option<Guid * CacheOutput<'ItemId, 'Item>>) input ->
+
+                        match input.CacheInput with
+                        | CreateItem(itemId, item) ->
+                            match state with
+                            | None ->
+                                Some item, CreateItemOnDB(itemId, Ok item)
+                            | Some _ -> 
+                                Some item, CreateItemOnDB(itemId, Error "Item already exists in cache")
+                            |> async.Return
+                            
+                        | ReadItem itemId ->
+                            match state with
+                            | None -> 
+                                input.CacheInput
+                                |> cacheInputToItemId
+                                |> readItemOnDatabase
+                            | Some item -> Ok item |> async.Return
+                            |> Async.map (fun result ->
+                                match result with
+                                | Ok item -> Some item, ReadItemOnDB(itemId, Ok item)
+                                | Error err -> None, ReadItemOnDB(itemId, Error err)
+                            )
+                            
+                        | UpdateItem(itemId, itemMsg) ->
+                            match state with
+                            | None -> 
+                                input.CacheInput
+                                |> cacheInputToItemId
+                                |> readItemOnDatabase
+                            | Some item -> Ok item |> async.Return
+                            |> Async.map (fun result ->
+                                match result with
+                                | Ok item ->
+                                    let updatedItem = updateItem itemMsg item
+
+                                    Some updatedItem, UpdateItemOnDB(itemId, Ok updatedItem)
+                                | Error err -> None, UpdateItemOnDB(itemId, Error err)
+                            )
+
+                        | DeleteItem itemId ->
+                            (None, DeleteItemOnDB(itemId, Ok()))
+                            |> async.Return
+                            
+                        |> Async.map (fun (itemOption, co) -> itemOption, Some (input.CorrelationId, co) )
+
+                )
+                (None,None)
+            >> AsyncSeq.toObservable
+            >> Observable.choose (fun ( _, processorOutputs) -> processorOutputs)
+
+
+        )
+
 let obsCache
     (createOrUpdateItemOnDatabase: 'Item -> IObservable<Result<'Item, string>>)
     (readItemOnDatabase: 'ItemId -> IObservable<Result<'Item, string>>)
